aa7de66a3e463fe3f0fd7a6fb021118e150166f9
Nikita Vylegzhanin <nikita@Nikitas-MacBook-Pro.local>
2020-08-05 14:08:57 +0300

    Init project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+  //
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
